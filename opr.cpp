12b96e190a424fa77a8c4b3f2a3a93aea3a4e262
princepatel451 <princepatel91929@gmail.com>
2025-11-02 21:25:35 +0530

    Subtraction added

--- opr.cpp
+++ opr.cpp
@@ -3,10 +3,14 @@ using namespace std;
 int add(int a, int b){
     return a + b;
 }
+int sub(int a, int b){
+    return abs(a - b);
+}
 int main(){
     int a,b;
     cout<<"Enter a number: ";
     cin>>a;
     cin>>b;
     cout<<add(a,b);
+    cout<<sub(a,b);
 }
